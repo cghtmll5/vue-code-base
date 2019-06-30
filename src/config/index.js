87403702ab16fb4a577bee7cb1aaf4ca564946b4
cg520 <793405168@qq.com>
2019-06-30 09:37:11 +0800

    213

--- src/config/index.js
+++ src/config/index.js
@@ -3,13 +3,13 @@ let baseUrl = '';
 let imgUrl = '';
 let deskey = "";
 if (process.env.NODE_ENV === 'development'){
-  baseUrl = "http://47.110.152.126:8020/auction/";
-  imgUrl = 'http://47.110.152.126/';
-  deskey = "mpT8PcFm4IvU+uZMHy3Tn0YjlGcQcc0zbC8DAXmMw13XcHGL7A7KTA==";
+  baseUrl = "";
+  imgUrl = '';
+  deskey = "";
 } else if (process.env.NODE_ENV === 'production') {
-  baseUrl = 'https://phone.youpaiwang.com/auction/';
-  imgUrl = 'https://image.youpaiwang.com/';
-  deskey = "sEv/0y/0uB6d4H48RBQwRW9d14010WWxYXsteygaOh/XcHGL7A7KTA==";
+  baseUrl = 'https.www.baidu.com';
+  imgUrl = 'https.www.baidu.com';
+  deskey = "https.www.baidu.com";
 }
 export  {
   baseUrl,
